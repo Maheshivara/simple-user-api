a9b459d980ebd709181bf45f5125793bf042c28a
Maheshivara <gabrimath@gmail.com>
2023-08-06 20:41:48 -0300

    Initial config to express listening

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+if (!process.env.PORT) {
+  console.error("PORT not found in the .env file");
+  process.exit(1);
+}
+let PORT: number;
+try {
+  PORT = parseInt(process.env.PORT as string, 10);
+} catch {
+  console.error("Invalid PORT in the .env file");
+  process.exit(1);
+}
+
+const api = express();
+api.use(cors());
+api.use(express.json());
+
+api.listen(PORT, () => console.log(`Listening on port ${PORT}`));
