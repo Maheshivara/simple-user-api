b8bc7d77c70a77960282d343d8844c86d3039201
Maheshivara <gabrimath@gmail.com>
2023-11-02 23:54:07 -0300

    Creating update password endpoint

--- src/user/user.routers.ts
+++ src/user/user.routers.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import { body, validationResult } from 'express-validator';
-import { login, createUser, updateUserInfo } from './user.services';
+import {
+  login,
+  createUser,
+  updateUserInfo,
+  updatePassword,
+} from './user.services';
 
 export const userRouters = express.Router();
 
@@ -21,7 +26,7 @@ userRouters.get(
         passwordHash: request.body.password,
       };
       const user = await login(loginData);
-      if (user === false || user === null) {
+      if (!user) {
         response.set('WWW-Authenticate', 'Basic');
         return response
           .status(401)
@@ -62,7 +67,7 @@ userRouters.post(
         phoneNumber: request.body.phoneNumber,
       };
       const user = await createUser(userData);
-      if (user === null) {
+      if (!user) {
         return response.status(409).json({ error: 'User already exist' });
       }
       return response.status(201).json({ newUser: user });
@@ -79,11 +84,11 @@ userRouters.put(
   '/',
   body('email').isEmail(),
   body('password').isString(),
-  body('name').optional().isString(),
-  body('userName').optional().isString(),
-  body('birthday').optional().isISO8601(),
-  body('location').optional().isString(),
-  body('phoneNumber').optional().isMobilePhone('any'),
+  body('newName').optional().isString(),
+  body('newUserName').optional().isString(),
+  body('newBirthday').optional().isISO8601(),
+  body('newLocation').optional().isString(),
+  body('newPhoneNumber').optional().isMobilePhone('any'),
   async (request, response) => {
     try {
       const result = validationResult(request);
@@ -95,37 +100,67 @@ userRouters.put(
         passwordHash: request.body.password,
       };
       const user = await login(userLogin);
-      if (user === false || user === null) {
+      if (!user) {
         response.set('WWW-Authenticate', 'Basic');
         return response
           .status(401)
           .json({ errors: 'Incorrect password or user not registered' });
       }
       if (
-        !request.body.name &&
-        !request.body.userName &&
-        !request.body.birthday &&
-        !request.body.location &&
-        !request.body.phoneNumber
+        !request.body.newName &&
+        !request.body.newUserName &&
+        !request.body.newBirthday &&
+        !request.body.newLocation &&
+        !request.body.newPhoneNumber
       ) {
         return response.status(400).json({ errors: 'No fields to update' });
       }
 
       const userUpdatedData = {
         email: request.body.email,
-        name: request.body.name ?? user.name,
-        userName: request.body.userName ?? user.userName,
+        name: request.body.newName ?? user.name,
+        userName: request.body.newUserName ?? user.userName,
         birthday:
-          request.body.birthday === undefined
+          request.body.newBirthday === undefined
             ? user.birthday
-            : new Date(request.body.birthday),
-        location: request.body.location ?? user.location,
-        phoneNumber: request.body.phoneNumber ?? user.phoneNumber,
+            : new Date(request.body.newBirthday),
+        location: request.body.newLocation ?? user.location,
+        phoneNumber: request.body.newPhoneNumber ?? user.phoneNumber,
       };
       const updatedUser = await updateUserInfo(userUpdatedData);
       return response.status(200).json({ updatedUser: updatedUser });
     } catch (error: any) {
       console.error(`User update error:\n${error.message}`);
+      return response.status(500).json({ error: 'Internal API error' });
+    }
+  }
+);
+
+userRouters.put(
+  '/password',
+  body('email').isEmail(),
+  body('password').isString(),
+  body('newPassword').isStrongPassword(),
+  async (request, response) => {
+    try {
+      const result = validationResult(request);
+      if (!result.isEmpty()) {
+        return response.status(400).json({ errors: result.array() });
+      }
+      const userLogin = {
+        email: request.body.email,
+        passwordHash: request.body.password,
+      };
+      const user = await updatePassword(userLogin, request.body.newPassword);
+      if (!user) {
+        response.set('WWW-Authenticate', 'Basic');
+        return response
+          .status(401)
+          .json({ errors: 'Incorrect password or user not registered' });
+      }
+      return response.status(200).json({ updatedUser: user });
+    } catch (error: any) {
+      console.error(`User password update error:\n${error.message}`);
       return response.status(500).json({ error: 'Internal API error' });
     }
   }
